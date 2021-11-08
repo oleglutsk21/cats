f8317fedb30c66c0a7c761eb1df776997898547e
oleglutsk21 <oleglutsk21@gmail.com>
2021-11-08 11:42:10 +0200

    Created base structure for custom module oleg

---
+++ web/modules/custom/oleg/src/Controller/OlegController.php
@@ -0,0 +1,23 @@
+<?php
+namespace Drupal\oleg\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+/**
+ * Provides route responses for the Example module.
+ */
+class OlegController extends ControllerBase {
+
+  /**
+   * Returns a simple page.
+   *
+   * @return array
+   *   A simple renderable array.
+   */
+  public function catsPage() {
+    return [
+      '#markup' => '',
+      ];
+  }
+
+};
